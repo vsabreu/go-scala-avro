59ab1401fa9b49b08a67ab8de234b6976eb328c0
Vinicius Abreu <vsa@daileon.local>
2018-01-13 01:00:10 -0200

    Extracted message code to another file. Few refactorings.

--- producer/main.go
+++ producer/main.go
@@ -1,55 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"math/rand"
 	"strconv"
 	"time"
 
 	"github.com/Shopify/sarama"
-	"github.com/linkedin/goavro"
 )
 
-type message struct {
-	MessageNumber int    `json:"msg_num"`
-	IP            string `json:"ip"`
-	CollectedAt   int64  `json:"collected_at"`
-	User          string `json:"user"`
-	RandomKey     string `json:"rand_key"`
-}
-
-var msgsNum = flag.Int("msgs", 0, "number of messages that will be produced to Kafka")
-
-const (
-	letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	kafkaBroker = "127.0.0.1:9092"
+var (
 	kafkaTopic  = "go-scala-avro"
-	ipMask      = "127.0.0.%d"
-	schema      = `
-	{
-	  "type": "record",
-	  "name": "message",
-	  "doc:": "A basic schema for storing message data",
-	  "namespace": "com.goscalaavro",
-	  "fields": [
-		{ "doc": "Message number", "type": "int", "name": "msg_num" },
-		{ "doc": "User IP", "type": "string", "name": "ip" },
-		{ "doc": "Date of collect", "type": "long", "name": "collected_at" },
-		{ "doc": "User name", "type": "string", "name": "user" },
-		{ "doc": "Random key", "type": "string", "name": "rand_key" }
-	  ]
-	}`
+	kafkaBroker = []string{"127.0.0.1:9092"}
+	msgsNum     = flag.Int("msgs", 0, "number of messages that will be produced to Kafka")
 )
 
 func init() {
 	flag.Parse()
-
 }
 
 func main() {
-	msgs := make(chan *message, *msgsNum)
+	msgs := make(chan *Message, *msgsNum)
 	done := make(chan bool)
 
 	go buildMessages(msgs)
@@ -58,87 +29,48 @@ func main() {
 	<-done
 }
 
-func buildMessages(msgs chan<- *message) {
+func buildMessages(msgs chan<- *Message) {
 	for i := 0; i < *msgsNum; i++ {
-		msgs <- &message{
-			MessageNumber: i,
-			IP:            fmt.Sprintf(ipMask, i),
-			CollectedAt:   time.Now().UTC().Unix(),
-			User:          randString(10),
-			RandomKey:     randString(20),
-		}
+		msgs <- NewMessage(i)
 	}
 	close(msgs)
 }
 
-func produceMessages(msgs <-chan *message, done chan<- bool) {
+func produceMessages(msgs <-chan *Message, done chan<- bool) {
 	for m := range msgs {
 		sendToKafka(m)
 	}
 	done <- true
 }
 
-func sendToKafka(msg *message) {
+func sendToKafka(msg *Message) {
 	c := sarama.NewConfig()
 	c.Producer.Retry.Max = 3
 	c.Producer.Return.Successes = true
 
-	now := strconv.Itoa(int(time.Now().Unix()))
-	pm := &sarama.ProducerMessage{
-		Topic: kafkaTopic,
-		Key:   sarama.StringEncoder(now),
-		Value: sarama.StringEncoder(avroEncodedMessage(msg)),
-	}
-
-	producer, err := sarama.NewAsyncProducer([]string{kafkaBroker}, c)
+	producer, err := sarama.NewAsyncProducer(kafkaBroker, c)
 	if err != nil {
 		panic(err)
 	}
 
-	defer func() {
-		if err := producer.Close(); err != nil {
-			panic(err)
-		}
-	}()
-
-	producer.Input() <- pm
+	defer producer.Close()
+	now := strconv.Itoa(int(time.Now().Unix()))
 
 ProducerLoop:
 	for {
 		select {
+		case producer.Input() <- &sarama.ProducerMessage{
+			Topic: kafkaTopic,
+			Key:   sarama.StringEncoder(now),
+			Value: sarama.StringEncoder(msg.ToAvro()),
+		}:
+
 		case err := <-producer.Errors():
 			panic(err)
 		case s := <-producer.Successes():
-			fmt.Printf("Success producing message %d to topic %s\n", msg.MessageNumber, s.Topic)
+			fmt.Printf("Success producing message %d to topic %s\n", msg.ID, s.Topic)
 			break ProducerLoop
+
 		}
 	}
-}
-
-func avroEncodedMessage(msg *message) []byte {
-	rec, err := goavro.NewCodec(schema)
-	if err != nil {
-		panic(err)
-	}
-
-	b, err := rec.BinaryFromNative(nil, msg.getMap())
-	if err != nil {
-		panic(err)
-	}
-	return b
-}
-
-func randString(size int) string {
-	b := make([]byte, size)
-	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
-	}
-	return string(b)
-}
-
-func (msg *message) getMap() map[string]interface{} {
-	var gen interface{}
-	b, _ := json.Marshal(msg)
-	json.Unmarshal(b, &gen)
-	return gen.(map[string]interface{})
 }
---
+++ producer/message.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"math/rand"
+	"time"
+
+	"github.com/linkedin/goavro"
+)
+
+const (
+	letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	schema  = `{"type":"record","name":"message","doc:":"A basic schema for storing message data",
+		"namespace":"com.goscalaavro","fields":[{"doc":"Message number","type":"int","name":"id"},
+		{"doc":"Date of collect","type":"long","name":"collected_at"},
+		{"doc":"User name","type":"string","name":"user"},{"doc":"Random key","type":"string","name":"key"}]}`
+)
+
+// Message represents a message for Kafka
+type Message struct {
+	ID          int    `json:"id"`
+	CollectedAt int64  `json:"collected_at"`
+	User        string `json:"user"`
+	Key         string `json:"key"`
+}
+
+// NewMessage returns a new Message
+func NewMessage(i int) *Message {
+	return &Message{
+		ID:          i,
+		CollectedAt: time.Now().UTC().Unix(),
+		User:        randString(10),
+		Key:         randString(20),
+	}
+}
+
+// ToAvro encodes a Message in Avro byte array
+func (msg *Message) ToAvro() []byte {
+	c, err := goavro.NewCodec(schema)
+	if err != nil {
+		panic(err)
+	}
+
+	b, err := c.BinaryFromNative(nil, msg.toMap())
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+// toMap transforms a Message struct in a map[string]interface{}
+func (msg *Message) toMap() map[string]interface{} {
+	var m interface{}
+
+	b, err := json.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+
+	json.Unmarshal(b, &m)
+	return m.(map[string]interface{})
+}
+
+func randString(size int) string {
+	b := make([]byte, size)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
