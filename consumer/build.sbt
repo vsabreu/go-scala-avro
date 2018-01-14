886be2f64e6427948b86f167c9d177d3fa156048
Vinicius Abreu <vsa@daileon.local>
2018-01-14 17:28:09 -0200

    Added avro4s to generate case classes based on avro schema

--- consumer/build.sbt
+++ consumer/build.sbt
@@ -3,4 +3,7 @@ name := "consumer"
 version := "1.0"
 
 scalaVersion := "2.12.4"
-        
+
+libraryDependencies ++= Seq(
+  "com.sksamuel.avro4s" %% "avro4s-core" % "1.8.0"
+)
--- consumer/src/main/scala/com/github/vsabreu/go_scala_avro/Consumer.scala
+++ consumer/src/main/scala/com/github/vsabreu/go_scala_avro/Consumer.scala
@@ -2,6 +2,7 @@ package com.github.vsabreu.go_scala_avro
 
 object Consumer {
   def main(args: Array[String]): Unit = {
-    println("Kafka messages will be consumed here")
+    val msg = Message(id = 1, collected_at = 1515957924, user = "user", key = "key")
+    println(msg)
   }
 }
