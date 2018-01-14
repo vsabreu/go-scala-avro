caaf9f6d25f5688dcfb04910b38af57b181d5857
Vinicius Abreu <vsa@daileon.local>
2018-01-14 21:24:15 -0200

    Added basic implementation of consumer project.

--- consumer/build.sbt
+++ consumer/build.sbt
@@ -1,9 +1,12 @@
 name := "consumer"
-
 version := "1.0"
-
 scalaVersion := "2.12.4"
 
+resolvers += "Confluent Maven" at "http://packages.confluent.io/maven/"
+
 libraryDependencies ++= Seq(
-  "com.sksamuel.avro4s" %% "avro4s-core" % "1.8.0"
+  "com.sksamuel.avro4s" %% "avro4s-core" % "1.8.0",
+  "org.apache.avro" % "avro" % "1.8.2",
+  "org.apache.kafka" % "kafka_2.12" % "1.0.0",
+  "io.confluent" % "kafka-avro-serializer" % "3.3.1"
 )
--- consumer/src/main/scala/com/github/vsabreu/go_scala_avro/Consumer.scala
+++ consumer/src/main/scala/com/github/vsabreu/go_scala_avro/Consumer.scala
@@ -1,8 +1,55 @@
 package com.github.vsabreu.go_scala_avro
 
-object Consumer {
+import java.util.Properties
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import kafka.consumer.{Consumer, ConsumerConfig, Whitelist}
+import kafka.serializer.StringDecoder
+import org.apache.kafka.common.serialization.StringDeserializer
+
+object Main {
   def main(args: Array[String]): Unit = {
-    val msg = Message(id = 1, collected_at = 1515957924, user = "user", key = "key")
-    println(msg)
+
+    val topic = "go-scala-avro-topic"
+
+    val stream = {
+      val props = getProperties()
+      val consumer = Consumer.create(new ConsumerConfig(props))
+
+      consumer.createMessageStreamsByFilter(
+        new Whitelist(topic), 1, new StringDecoder(), new StringDecoder())(0)
+    }
+
+    lazy val iterator = stream.iterator()
+
+    printf("Consumer ready to consume messages from topic %s.\n", topic)
+
+    while(true) {
+      if(hasNext) {
+        val msg = iterator.next().message()
+        println("Got message: " + msg.toString)
+      }
+    }
+
+    def hasNext: Boolean = {
+      try {
+        iterator.hasNext
+      }
+      catch {
+        case ex: Exception => ex.printStackTrace()
+          false
+      }
+    }
+  }
+
+  def getProperties(): Properties = {
+    val p = new Properties()
+    p.setProperty("bootstrap.servers", "127.0.0.1:9092")
+    p.setProperty("zookeeper.connect", "127.0.0.1:2181")
+    p.setProperty("group.id", "scalagroup")
+    p.setProperty("auto.commit.enable", "true")
+    p.setProperty("key.deserializer", classOf[StringDeserializer].getName)
+    p.setProperty("value.deserializer", classOf[KafkaAvroDeserializer].getName)
+    p
   }
 }
