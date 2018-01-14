2fb4b4d22d9e03b8c55e640f0d7cce87217500ac
Vinicius Abreu <vsa@daileon.local>
2018-01-14 01:33:55 -0200

    Added initial consumer project. Added items to gitignore

---
+++ consumer/build.sbt
@@ -0,0 +1,6 @@
+name := "consumer"
+
+version := "1.0"
+
+scalaVersion := "2.12.4"
+        
---
+++ consumer/src/main/scala/com/github/vsabreu/go_scala_avro/Consumer.scala
@@ -0,0 +1,7 @@
+package com.github.vsabreu.go_scala_avro
+
+object Consumer {
+  def main(args: Array[String]): Unit = {
+    println("Kafka messages will be consumed here")
+  }
+}
